5625e11886fb87742d4a8e280c14827d8916432b
Munna Ahmed <munnaahmed2025@gmail.com>
2023-02-19 14:22:28 +0600

    added 11-fs-async

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,12 +1,12 @@
 const { readFileSync, writeFileSync } = require("fs");
 // const fs = require("fs");
 // fs.readFileSync
-
+console.log("start");
 // read file
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
-console.log(first, second);
+// console.log(first, second);
 
 // create new file
 writeFileSync(
@@ -14,3 +14,6 @@ writeFileSync(
     `Here is the result : ${first}, ${second}`,
     { flag: "a" }
 );
+
+console.log("done with this task");
+console.log("starting the next one");
