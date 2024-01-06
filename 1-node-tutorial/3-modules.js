8ec9da788c70d2165df043512c65b956c1bc016b
Munna Ahmed <munnaahmed2025@gmail.com>
2024-01-07 02:38:34 +0600

    commit

--- 1-node-tutorial/3-modules.js
+++ 1-node-tutorial/3-modules.js
@@ -1,8 +1,8 @@
 // CommonJS, every file in module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require("./4-names"); //file location in brackets
-const sayHi = require("./5.utils");
-const data = require("./6-alternative-flavor");
+const names = require("./names"); //file location in brackets
+const sayHi = require("./utils");
+const data = require("./alternative-flavor");
 require("./7-mind-granade"); // execute not export
 
 sayHi("susan");
