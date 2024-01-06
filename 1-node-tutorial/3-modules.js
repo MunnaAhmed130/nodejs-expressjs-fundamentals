a774ae9b087f8567c7b834438ede0bcf8102180b
Munna Ahmed <munnaahmed2025@gmail.com>
2024-01-07 02:48:08 +0600

    commit

--- 1-node-tutorial/3-modules.js
+++ 1-node-tutorial/3-modules.js
@@ -1,8 +1,9 @@
 // CommonJS, every file in module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require("./names"); //file location in brackets
-const sayHi = require("./utils");
-const data = require("./alternative-flavor");
+const names = require("./export/names"); //file location in brackets
+
+const sayHi = require("./export/utils");
+const data = require("./export/alternative-flavor");
 require("./7-mind-granade"); // execute not export
 
 sayHi("susan");
--- 1-node-tutorial/export/names.js
+++ 1-node-tutorial/export/names.js
@@ -1,10 +1,14 @@
 // local
 const secret = "SUPER SECRET";
+
 // share
 const john = "john";
 const peter = "peter";
 
-// console.log(module);
+// module is a object with exports and other property
+console.log(module);
 
 // export default
 module.exports = { john, peter }; // export default multiple value
+
+console.log(module);
